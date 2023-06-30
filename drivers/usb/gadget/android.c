62e737e4ec1718e50eefbe24acac7b68b3f7279a
Zillion <77107077+DevZillion@users.noreply.github.com>
2023-06-30 13:53:20 +0200

    usb: HID patch

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -42,6 +42,7 @@
 #endif
 #include "f_mass_storage.c"
 #define USB_ETH_RNDIS y
+<<<<<<< HEAD
 #include "f_diag.c"
 #include "f_qdss.c"
 #include "f_rmnet_smd.c"
@@ -58,6 +59,9 @@
 #include "f_ccid.c"
 #include "f_mtp.c"
 #include "f_accessory.c"
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
 #include "f_rndis.c"
 #include "rndis.c"
 #include "f_qc_ecm.c"
@@ -2796,6 +2800,41 @@ static struct android_usb_function uasp_function = {
 	.bind_config	= uasp_function_bind_config,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
 	&mbim_function,
@@ -2822,8 +2861,12 @@ static struct android_usb_function *supported_functions[] = {
 #ifdef CONFIG_SND_PCM
 	&audio_source_function,
 #endif
+<<<<<<< HEAD
 	&uasp_function,
 	&charger_function,
+=======
+    &hid_function,
+>>>>>>> 20caacfc250 (usb: HID patch)
 	NULL
 };
 
@@ -3203,6 +3246,8 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 				  struct android_configuration, list_item);
 		free_android_config(dev, conf);
 	}
+    /* HID driver always enabled, it's the whole point of this kernel patch */
+    android_enable_function(dev, "hid");
 
 	mutex_unlock(&dev->mutex);
 
